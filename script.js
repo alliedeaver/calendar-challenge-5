7b65480b471eac88289ae34ae2d3a7d84f6d4300
alliedeaver <adeav97@gmail.com>
2023-08-31 19:56:40 -0700

    additions to js

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-var rootEl = $('#root');
+
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-let display = document.querySelector('.display-3')
-let leadApp = document.querySelector('.lead')
-let currentDay = document.getElementById('#currentDay')
-let hourTabs = document.querySelector('.container-fluid px-5')
-let past = document.querySelector('.past')
-let present = document.querySelector('.present')
-let future = document.querySelector('.future')
-let hour9 = document.getElementById('#hour-9')
-let timeBlock = document.querySelector('.row time-block past')
-let column2 = document.querySelector('.col-2 col-md-1 hour text-center py-3')
-let columnScrip = document.querySelector('.col-8 col-md-10 description')
+let display = document.querySelector('#display-3')
+let leadApp = document.querySelector('#lead')
+let currentDay = document.querySelector('#currentDay')
+let hourTabs = document.querySelector('#container-fluid px-5')
+let past = document.querySelector('#past')
+let present = document.querySelector('#present')
+let future = document.querySelector('#future')
+let hour9 = document.querySelector('#hour-9')
+let timeBlock = document.querySelector('#row time-block past')
+let column2 = document.querySelector('#col-2 col-md-1 hour text-center py-3')
+let columnScrip = document.querySelector('#col-8 col-md-10 description')
 
 
-$(function () {
+dayjs().format("HH")
+
+$('#currentDay').text ((dayjs().format("dddd MMM D, YYYY")));
+
+
+
+$(".timeblock").each(function() {   
+ currentHour > timeBlock) {
+    $(this).addClass("past")
+}
+
+if (currentHour == timeBlock) {
+    $(this).addClass("present")
+}
+if (currentHour < timeBlock) {
+    $(this).addClass("future")
+}
+
+
+
+    $(".saveBtn").on("click", function () {
+
+    })
+    
+
+
+
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
-    //
+
+
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
@@ -34,4 +62,3 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
